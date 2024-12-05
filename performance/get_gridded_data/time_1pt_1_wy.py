e711c66fab06c8a7c27d3f0f751e30446b6d05b1
wh3248 <wh3248@princeton.edu>
2024-12-05 14:14:13 -0500

    add initial github actions and a hello world test

---
+++ performance/get_gridded_data/time_1pt_1_wy.py
@@ -0,0 +1,13 @@
+"""
+Performance test for the scenario to 1 point for 1 water year
+using hf_hydrodata.
+"""
+
+def main():
+    """Execute scenario and collect timing."""
+
+    print("SCENARIO 1 - 1 point for 1 water year.")
+    
+
+if __name__ == "__main__":
+    main()
