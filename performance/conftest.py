0c2664483a3f8793a5646a38dc38bbe0ef9dbd87
wh3248 <wh3248@princeton.edu>
2025-01-04 11:01:41 -0500

    use 2004 as default wy

--- performance/conftest.py
+++ performance/conftest.py
@@ -5,7 +5,7 @@ def pytest_addoption(parser):
     """Add supported options"""
 
     parser.addoption("--cache", action="store", default="cold")
-    parser.addoption("--wy", action="store", default="1997")
+    parser.addoption("--wy", action="store", default="2002")
     parser.addoption("--wy_month", action="store", default="02")
     parser.addoption("--comment", action="store", default="")
     parser.addoption("--cpus", action="store", default="8")
