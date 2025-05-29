f6aca4ff2e2b057c44f259011396cba137dbcb91
wh3248 <wh3248@princeton.edu>
2025-05-29 14:37:06 -0400

    use pypi version of subsettools

--- performance/test_subset_forcing_years.py
+++ performance/test_subset_forcing_years.py
@@ -21,22 +21,22 @@ def test_scenario(request):
     This downloads 248 forcing files (1 month, 8 vars) which are removed after the test executes.
     """
 
-    local_remote = utils.register_email_pin("private")
+    #local_remote = utils.register_email_pin("private")
     wy = request.config.getoption("--wy")
     wy = int(wy)
-    data_dir = "/scratch/wh3248/forcing_years"
+    data_dir = "./test_dir"
     os.makedirs(data_dir, exist_ok=True)
     # Do not run this normally because it uses too much disk space
-    return
+    #return
     t0 = time.time()
     _execute_scenario(data_dir, wy)
     t1 = time.time()
     duration = round(t1 - t0, 2)
     scenario_name = "subset_forcing_years"
-    utils.write_log(scenario_name, request, local_remote, duration)
+    print(f"Download forcing for 5 years in {duration} seconds.")
+    #utils.write_log(scenario_name, request, local_remote, duration)
     assert os.path.exists(f"{data_dir}/CW3E.Press.000001_to_000024.pfb")
     shutil.rmtree(f"{data_dir}")
-    print(f"download then deleted directory {data_dir}")
     assert not os.path.exists("{data_dir}/CW3E.Press.000001_to_000024.pfb")
 
 
@@ -44,10 +44,10 @@ def _execute_scenario(data_dir, wy):
     """Execute the scenario to be tested"""
 
     forcing_paths = st.subset_forcing(
-        ij_bounds=(2865, 1143, 2923, 1184),
+        ij_bounds = (2909, 1385, 2921, 1403),   # Mickey Rush's bounds
         grid="conus2",
         start=f"{wy}-10-01",
-        end=f"{wy+2}-10-01",
+        end=f"2022-10-01",
         dataset="CW3E",
         write_dir=data_dir,
     )
